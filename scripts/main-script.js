fcd444c2b24ea0098cbfda97512f7a624114ea93
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2022-10-06 18:07:54 +0100

    change button to a nicer looking one

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -2,14 +2,13 @@ let container = document.getElementById('main-container');      //Grab object of
 
 let headerSection = document.getElementById('header-section'); //Create button to generate the custom grid 
 let btn2 = document.createElement('button');
-btn2.style.cssText = "background-image: url(/etch-a-sketch/img/rainbow-button2.png); background-size: 100%; height: 50px; width: 100px;";
-btn2.textContent = "Custom Div";
+btn2.textContent = "Let's have some fun!";
 headerSection.appendChild(btn2);
        
 btn2.addEventListener('click', () => {      //Add listener on click, get input from user and validate
     let answer = prompt("Yo, what was that number of cells ya wanted in the grid?");
-    while (answer == null || Number.isInteger(Number(answer)) == false || Number(answer) > 100) {
-        answer = prompt("Sorry buddy. It has to be a number (not a word), lower than 64")
+    while (answer == null || Number.isInteger(Number(answer)) == false || Number(answer) > 65) {
+        answer = prompt("Sorry buddy. It has to be a number (not a word), and it needs to be maximum 64")
     }
     createDivs(answer);
 
@@ -39,7 +38,7 @@ function createDivs (num) {        //Main function to create div grid
         let div = document.createElement('div');
         div.setAttribute("class", "divs");
         div.addEventListener('mouseover', () => {rainbowTime(div)})     //adding event listener for on hover
-        div.style.cssText += "display:flex; max-width:" + size +"px; width:" +size+"px; height: " +size+"px; max-height: " + size +"px; background-color:grey;border-color: black; border-width: 2px; filter: brightness(100%);" //max width is 51 and declared width is 50 to allow for a 1px gap
+        div.style.cssText += "display:flex; max-width:" + size +"px; width:" +size+"px; height: " +size+"px; max-height: " + size +"px; background-color:pink;border-color: black; border-width: 2px; filter: brightness(100%);" //max width is 51 and declared width is 50 to allow for a 1px gap
         container.appendChild(div);
     }
 }
