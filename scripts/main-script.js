dc050691bdf08142b1ef629727628f80a4c206ff
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2022-10-06 16:40:38 +0100

    add random colour background on hover

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -16,7 +16,10 @@ btn2.addEventListener('click', () => {      //Add listener on click, get input f
 }); 
     
 function rainbowTime (div_name) {       //Function to add on hover class
-    div_name.setAttribute("class", "divs on-hover");
+    let randomNumber = Math.floor(Math.random() * 256);
+    let randomRgb = "rgb("+randomNumber+","+randomNumber+","+randomNumber+")";
+    // div_name.setAttribute("class", "divs on-hover");
+    div_name.style.cssText += "background-image: linear-gradient(90deg,"+randomRgb+" 0%, "+randomRgb+" 49%, "+randomRgb+" 80%, "+randomRgb+" 100%); animation:slidebg 5s linear infinite;";
 }
 
 
@@ -28,8 +31,8 @@ function createDivs (num) {        //Main function to create div grid
         let size = 960/num;     //calculate the size of individual div
         let div = document.createElement('div');
         div.setAttribute("class", "divs");
-        div.addEventListener('mouseover', () => {rainbowTime(div)})     //adding event listener for on hoover
-        div.style.cssText = "display:flex; max-width:" + size +"px; width:" + (size-1) +"px; height: " + (size-1)+"px; max-height: " + size +"px; background-color:grey;border-color: black; border-width: 2px" //max width is 51 and declared width is 50 to allow for a 1px gap
+        div.addEventListener('mouseover', () => {rainbowTime(div)})     //adding event listener for on hover
+        div.style.cssText += "display:flex; max-width:" + size +"px; width:" +size+"px; height: " +size+"px; max-height: " + size +"px; background-color:grey;border-color: black; border-width: 2px" //max width is 51 and declared width is 50 to allow for a 1px gap
         container.appendChild(div);
     }
 }
